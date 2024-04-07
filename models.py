7e5154da514edc3d7dd07fa8887c4ffaaec9748f
Faithlin Hoe <faithlin07@gmail.com>
2024-04-07 20:22:42 +0800

    tidy and cleanup

--- models.py
+++ models.py
@@ -2,36 +2,46 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class UserData(db.Model):
-    __tablename__ = 'User Data'  
-    id = db.Column('UserID', db.Integer, primary_key=True)  
-    email = db.Column('Email', db.String(255), unique=True, nullable=False)
-    password = db.Column('Password', db.String(255), nullable=False)
-    role = db.Column('UserRole', db.String(50), nullable=False)
+    __tablename__ = "User Data"
+    id = db.Column("UserID", db.Integer, primary_key=True)
+    email = db.Column("Email", db.String(255), unique=True, nullable=False)
+    password = db.Column("Password", db.String(255), nullable=False)
+    role = db.Column("UserRole", db.String(50), nullable=False)
 
     def __init__(self, email, password, role):
         self.email = email
         self.password = password
         self.role = role
 
+
 class QueryResult(db.Model):
-    __tablename__ = 'Query Results'
-    QResultID = db.Column('QResultID',db.Integer, primary_key=True)
-    UserID = db.Column('UserID', db.Integer, db.ForeignKey('User Data.UserID'), nullable=False)
-    Query = db.Column('Query', db.Text, nullable=False)
+    __tablename__ = "Query Results"
+    QResultID = db.Column("QResultID", db.Integer, primary_key=True)
+    UserID = db.Column(
+        "UserID", db.Integer, db.ForeignKey("User Data.UserID"), nullable=False
+    )
+    Query = db.Column("Query", db.Text, nullable=False)
 
     def __init__(self, UserID, Query):
         self.UserID = UserID
         self.Query = Query
 
+
 class MedicalCondition(db.Model):
-    __tablename__ = 'Medical Conditions'
-    MConditionID = db.Column('MConditionID', db.Integer, primary_key=True)
-    QResultID = db.Column('QResultID', db.Integer, db.ForeignKey('Query Results.QResultID'), nullable=False)
-    Name = db.Column('Name', db.Text)
-    justification = db.Column('Justification', db.Text)
-    TreatmentSuggestion = db.Column('TreatmentSuggestion', db.Text)
-    Reference = db.Column('Reference', db.Text)
+    __tablename__ = "Medical Conditions"
+    MConditionID = db.Column("MConditionID", db.Integer, primary_key=True)
+    QResultID = db.Column(
+        "QResultID",
+        db.Integer,
+        db.ForeignKey("Query Results.QResultID"),
+        nullable=False,
+    )
+    Name = db.Column("Name", db.Text)
+    justification = db.Column("Justification", db.Text)
+    TreatmentSuggestion = db.Column("TreatmentSuggestion", db.Text)
+    Reference = db.Column("Reference", db.Text)
 
     def __init__(self, QResultID, Name, justification, TreatmentSuggestion, Reference):
         self.QResultID = QResultID
@@ -40,10 +50,16 @@ class MedicalCondition(db.Model):
         self.TreatmentSuggestion = TreatmentSuggestion
         self.Reference = Reference
 
+
 class Report(db.Model):
-    __tablename__ = 'Reports'
-    ReportID = db.Column('ReportID', db.Integer, primary_key=True)
-    QResultID = db.Column('QResultID', db.Integer, db.ForeignKey('Query Results.QResultID'), nullable=False)
+    __tablename__ = "Reports"
+    ReportID = db.Column("ReportID", db.Integer, primary_key=True)
+    QResultID = db.Column(
+        "QResultID",
+        db.Integer,
+        db.ForeignKey("Query Results.QResultID"),
+        nullable=False,
+    )
 
     def __init__(self, QResultID):
         self.QResultID = QResultID
--- temp.py
+++
@@ -1,6 +0,0 @@
-from cryptography.fernet import Fernet
-
-# Generate a key
-key = Fernet.generate_key()
-print(key)
-# Store this key securely! You will use it to create a Fernet instance later.
